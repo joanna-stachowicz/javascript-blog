846a292fa54d9a34ea818f2b2ec316c0ceefa484
Joanna Stachowicz <joannastachowicz19@gmail.com>
2020-06-24 00:01:19 +0200

    Add authorClickHandler function

--- js/script.js
+++ js/script.js
@@ -316,4 +316,54 @@
 
   generateAuthors();
 
+  const authorClickHandler = function (event) {
+
+    /* prevent default action for this event */
+
+    event.preventDefault();
+
+    /* make new constant named "clickedElement" and give it the value of "this" */
+
+    const clickedElement = this;
+
+    /* make a new constant "href" and read the attribute "href" of the clicked element */
+
+    const href = clickedElement.getAttribute('href');
+
+    console.log(href); // #author-breakfast
+
+    /* make a new constant "author" and extract author from the "href" constant */
+
+    const author = href.replace('#author-', '');
+
+    console.log(author); // breakfast
+
+    /* execute function "generateTitleLinks" with article selector as argument */
+
+    generateTitleLinks('[data-author="' + author + '"]');
+
+  };
+
+  const addClickListenersToAuthors = function () {
+
+    /* find all links to authors */
+
+    const allAuthorLinks = document.querySelectorAll('a[href^="#author-"]');
+
+    /* START LOOP: for each link */
+
+    for (let authorLink of allAuthorLinks) {
+
+      /* add authorClickHandler as event listener for that link */
+
+      authorLink.addEventListener('click', authorClickHandler);
+
+      /* END LOOP: for each link */
+
+    }
+
+  };
+
+  addClickListenersToAuthors();
+
 }
